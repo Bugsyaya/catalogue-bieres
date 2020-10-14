409412190cc6c91c1fd2cae84adceaf6a48d5d0d
Christophe Dos Santos Neto <christophe.dos-santos-neto@imt-atlantique.net>
2020-10-14 13:50:54 +0200

    Fixed changes asked on pull request

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,8 +2,3 @@ document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems);
 });
-
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.datepicker');
-    var instances = M.Datepicker.init(elems);
-});
